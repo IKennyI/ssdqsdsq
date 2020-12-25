5ab9a09784d17187dcf601e5340f365ac65477d1
test <testt>
2020-12-25 19:35:56 +0100

    Initial Commit

---
+++ ssdqsdsq/ssdqsdsqApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssdqsdsqApp.swift
+//  ssdqsdsq
+//
+//  Created by Léandre Kahn on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ssdqsdsqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
